96d62622c9582549ba252adebd4f37ccf37e7ca3
Leon Pahole <leon.pahole@gmail.com>
2022-06-11 00:53:00 +0200

    Add seo to 404 page

--- src/components/Page/NotFoundPage/NotFoundPage.tsx
+++ src/components/Page/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,12 @@
+import { SEO } from "components/seo/SEO";
 import { Link } from "gatsby";
 import React from "react";
 
 export const NotFoundPage = () => {
   return (
     <>
+      <SEO />
+
       <h1 className="site-heading">Not found</h1>
 
       <p>The requested page was not found.</p>
